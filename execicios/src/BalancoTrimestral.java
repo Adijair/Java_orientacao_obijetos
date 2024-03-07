d52acac413cec89785cacecb4bb75592a2c662c8
Adijair <adijairp05@gmail.com>
2024-03-07 11:45:59 -0300

    execicio 3.3

---
+++ execicios/src/BalancoTrimestral.java
@@ -0,0 +1,12 @@
+public class BalancoTrimestral { //criar classe
+    public static void main(String[] args) { //Dentro do metodo main declaras as variaveis
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+        int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco; //faze a soma
+
+        System.out.println("Gastos do trimestre: R$" + gastosTrimestre);
+        int mediaPorMes = + gastosTrimestre / 3; //faze a media aritimetica
+        System.out.println("Média mensal: R$" + mediaPorMes);
+    }
+}
